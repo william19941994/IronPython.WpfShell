ab9181cfe134a81b92c507cc88147a13c010c044
william19941994 <59403632+william19941994@users.noreply.github.com>
2023-09-02 22:59:04 +0800

    增加个图片

--- IronPython.WpfShell.Test/bin/Debug/net5.0-windows/test1.py
+++ IronPython.WpfShell.Test/bin/Debug/net5.0-windows/test1.py
@@ -1,10 +1,10 @@
 #import numpy as np  # injected by c# code 
-import os
+import os  # import file in lib.zip (in search path)
 os.path.abspath(__file__)
 
 a1=[1,2,3]
-a2=np.full((2,5),88)
-a3=np.sum(a2)
-print(a3) 
+a2=np.full((2,5),88)    # np is injected by c# code not need import. 
+a3=np.sum(a2) 
+print(a3)  # print 
 
-dir()
+dir() # not print in file, only print in command line input.
